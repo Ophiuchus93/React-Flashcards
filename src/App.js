bea140d17a30b248059e66a3a8b77eaa6b2f2525
Scott (DC)  Christensen <schristensen10@gmail.com>
2019-09-12 19:28:55 -0600

    Tried to add routes

--- src/App.js
+++ src/App.js
@@ -1,57 +1,9 @@
 import React from 'react';
-import { Container, Header, } from "semantic-ui-react";
-import Flashcards from './Flashcards'
-import FlashcardForm from "./FlashcardForm"
+import { Route, } from 'react-router-dom';
+import Flashcards from './Flashcards';
 
+const App = () => (
+  <Route exact path="/flashcards" component={Flashcards} />
+);
 
-class App extends React.Component {
-  state = {
-    flashcards: [
-      { id: 1, front: "What is state?", back: "An object of an array of objects to hold data." },
-      { id: 2, front: "What are props?", back: "Props are used to pass data from a parent to the child." },
-      { id: 3, front: "What are components?", back: "Components are like custom, reusable HTML elements." },
-      { id: 4, front: "What are the 2 kinds of components?", back: "Functional and class." },
-      { id: 5, front: "What data type is state?", back: "Always an object." },
-    ],
-    showForm: true,
-  };
-
-  getId = () => {
-    // NOTE We are just using this as a helper function for id's since we aren't using a db yet
-    return Math.floor((1 + Math.random()) * 10000);
-  };
-
-  addFlashcard = (flashcardData) => {
-    let flashcard = { id: this.getId(), ...flashcardData, };
-    this.setState({ flashcards: [...this.state.flashcards, flashcard] });
-  };
-
-  removeFlashcard = (id) => {
-    const flashcards = this.state.flashcards.filter(flashcard => {
-      if (flashcard.id !== id)
-        return flashcard
-    });
-    this.setState({ flashcards: [...flashcards,] });
-  }
-
-  toggleForm = () => this.setState({ showForm: !this.state.showForm, });
-
-  render() {
-    return (
-      <Container style={{ paddingTop: "25px" }}>
-        <Header as="h1">React Flashcards</Header>
-        <br />
-        <FlashcardForm add={this.addFlashcard} />
-        <br />
-        <Flashcards
-          flashcards={this.state.flashcards}
-          remove={this.removeFlashcard}
-          toggle={this.toggleForm}
-          showForm={this.state.showForm}
-        />
-      </Container>
-    );
-  };
-};
-
-export default App;
+export default App
--- src/Flashcards.js
+++ src/Flashcards.js
@@ -1,73 +1,56 @@
-import React from "react"
-import { Button, Table, } from "semantic-ui-react";
+import React from 'react';
+import { Container, Header, } from "semantic-ui-react";
+import FlashcardForm from "./FlashcardForm"
 
 
-const Flashcards = ({flashcards, remove, toggle, showForm}) => {
+class Flashcards extends React.Component {
+  state = {
+    flashcards: [
+      { id: 1, front: "What is state?", back: "An object of an array of objects to hold data." },
+      { id: 2, front: "What are props?", back: "Props are used to pass data from a parent to the child." },
+      { id: 3, front: "What are components?", back: "Components are like custom, reusable HTML elements." },
+      { id: 4, front: "What are the 2 kinds of components?", back: "Functional and class." },
+      { id: 5, front: "What data type is state?", back: "Always an object." },
+    ],
+    showBack: true,
+  };
 
-  const renderCards = () => {
-    return flashcards.map( flashcard => 
-      <>
-      <Table.Row key={flashcard.id} {...flashcard} remove={remove}>
-        
-        <Table.Cell>{flashcard.front}</Table.Cell>
-        <Table.Cell>
-          <Button color="green" onClick={toggle}>
-            Flip
-          </Button>
-          <Button color="red" onClick={() => remove(flashcard.id)}>
-          Delete
-          </Button>
-        </Table.Cell>
-      </Table.Row>
-      </>
-    )
+  getId = () => {
+    // NOTE We are just using this as a helper function for id's since we aren't using a db yet
+    return Math.floor((1 + Math.random()) * 10000);
+  };
+
+  addFlashcard = (flashcardData) => {
+    let flashcard = { id: this.getId(), ...flashcardData, };
+    this.setState({ flashcards: [...this.state.flashcards, flashcard] });
+  };
+
+  removeFlashcard = (id) => {
+    const flashcards = this.state.flashcards.filter(flashcard => {
+      if (flashcard.id !== id)
+        return flashcard
+    });
+    this.setState({ flashcards: [...flashcards,] });
   }
 
-  const renderCardsBack = () => {
-    return flashcards.map( flashcard => 
-      <>
-      <Table.Row key={flashcard.id} {...flashcard} remove={remove}>
-        
-        <Table.Cell>{flashcard.back}</Table.Cell>
-        <Table.Cell>
-          <Button color="green" onClick={toggle}>
-            Flip
-          </Button>
-          <Button color="red" onClick={() => remove(flashcard.id)}>
-          Delete
-          </Button>
-        </Table.Cell>
-      </Table.Row>
-      </>
-    )
-  }
+  toggleForm = () => this.setState({ showBack: !this.state.showBack, });
 
-  return showForm ? (
-  <Table celled padded>
-  <Table.Header>
-    <Table.Row>
-      <Table.HeaderCell>Front</Table.HeaderCell>
-    </Table.Row>
-  </Table.Header>
-
-  <Table.Body>
-  {renderCards()}
-  </Table.Body>
-  </Table>
-  ) : (
-    <Table celled padded>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Back</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
-  
-    <Table.Body>
-    {renderCardsBack()}
-    </Table.Body>
-    </Table>
-  )
+  render() {
+    return (
+      <Container style={{ paddingTop: "25px" }}>
+        <Header as="h1">React Flashcards</Header>
+        <br />
+        <FlashcardForm add={this.addFlashcard} />
+        <br />
+        <Flashcards
+          flashcards={this.state.flashcards}
+          remove={this.removeFlashcard}
+          toggle={this.toggleForm}
+          showBack={this.state.showBack}
+        />
+      </Container>
+    );
+  };
 };
-
 
 export default Flashcards;
